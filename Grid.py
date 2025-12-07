cb9e98a8339e9d59b70feb8d26247dd3395a2861
Arda Demirbilek <ardademirbilek05@gmail.com>
2025-12-07 11:23:12 +0100

    Add draw_template function to the Grid Class

--- Grid.py
+++ Grid.py
@@ -1,6 +1,6 @@
 import pygame
 from Node import Node
-from settings import SIDE, ROWS
+from settings import SIDE, ROWS, background_color, grid_color
 
 class Grid:
     def __init__(self):
@@ -14,3 +14,18 @@ class Grid:
             self.nodes.append([])
             for j in range(self.total_rows):
                 self.nodes[i].append(Node(i, j, i*self.node_side, j*self.node_side))
+
+    def draw_template(self, win):
+        win.fill(background_color)
+        # Each node is drawn
+        for row in self.nodes:
+            for node in row:
+                node.draw(win, self.node_side)
+        for i in range(self.total_rows):  # Vertical Lines of the Grid
+            pygame.draw.line(win, grid_color,
+                             (0, i * self.node_side), (self.side, i * self.node_side))
+            for j in range(self.total_rows):  # Horizontal Lines of the Grid
+                pygame.draw.line(win, grid_color,
+                                 (j * self.node_side, 0), (j * self.node_side, self.side))
+        pygame.display.update()
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
-from settings import SIDE
+from settings import SIDE, ROWS
+from Grid import Grid
+from Node import Node
 
 WIN = pygame.display.set_mode((SIDE, SIDE))
 pygame.display.set_caption("A* Path Finding Algorithm")
@@ -8,7 +10,9 @@ pygame.display.set_caption("A* Path Finding Algorithm")
 def main():
     start, end = None, None
     run, started = True, False
+    grid = Grid()
     while run:
+        grid.draw_template(WIN)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
